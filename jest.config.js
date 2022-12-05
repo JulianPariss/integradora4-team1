ae015e1532234ec6348ba996b544d2a3d1b61590
Joan <joandv.gil@gmail.com>
2022-12-05 20:40:07 -0300

    RTL

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
   moduleNameMapper: {
     "\\.(css|less|scss)$": "identity-obj-proxy"
   },
-  setupFilesAfterEnv: ["<rootDir>/src/jest-setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   testEnvironment: "jsdom"
 };
